62ea6aa29620874d68b15d8f3d78c5bd912866d2
A.Alexandrovich <aalexandrovich.hireme@gmail.com>
2022-11-22 10:48:43 +0400

    Update comments

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -3,9 +3,8 @@ package notify
 import domain "parser/internal/domain/models"
 
 type Notifier interface {
-	//
 	// `args` are specific for every Notifier impl.
-	// Firstly, look into concrete implementation args
+	// Firstly, look into the concrete implementation's args
 	// e.g. TelegramNotifier has it's own args rules...
 	Notify(ad *domain.Advert, args ...interface{}) error
 }
