a3b1afa62b2d30b8619835297d78fb17ab589eb8
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-11-27 20:17:18 +0300

    update ci, fixed another race condition

--- internal/parser/ring_parser.go
+++ internal/parser/ring_parser.go
@@ -13,14 +13,13 @@ type RingParser struct {
 	// List of URLs to parse
 	targets   []string
 	targetlen int32
+	offset    int32
 
 	// Keep track of what urls are targets. See RingParser.AddTarget
-	urls   map[string]struct{}
-	offset int32
+	urls map[string]struct{}
 
 	// Protect data
 	mu *sync.RWMutex
-	wg *sync.WaitGroup
 
 	parser Parser
 
@@ -28,24 +27,18 @@ type RingParser struct {
 	timeout time.Duration
 	timer   timer.Timer
 
-	// 1 - running
-	// 0 - stopped
-	state uint32
-
 	out      chan *ParseResult
 	shutdown chan struct{}
 }
 
 func NewRingParser(parser Parser, timer timer.Timer, parsingTimeout time.Duration, queueLength int) *RingParser {
 	return &RingParser{
+		offset:   0,
 		parser:   parser,
 		timer:    timer,
 		timeout:  parsingTimeout,
-		offset:   0,
 		mu:       new(sync.RWMutex),
-		wg:       new(sync.WaitGroup),
 		targets:  make([]string, 0),
-		state:    1,
 		urls:     make(map[string]struct{}),
 		shutdown: make(chan struct{}),
 		out:      make(chan *ParseResult, queueLength),
@@ -58,17 +51,11 @@ func (rp *RingParser) Run(interval time.Duration) {
 }
 
 func (rp *RingParser) Close() {
-	// Set state to 'stopped'
-	atomic.StoreUint32(&rp.state, 0)
-
-	// Wait for all parsing goroutines to end
-	rp.wg.Wait()
-	// Signal that no more updates are sent
-	close(rp.out)
-
 	// Stop emitting intervals
 	rp.timer.Stop()
 
+	// Trigger shutdown
+	close(rp.shutdown)
 }
 
 func (rp *RingParser) Out() <-chan *ParseResult {
@@ -116,34 +103,15 @@ func (rp *RingParser) parse() {
 		atomic.AddInt32(&rp.offset, 1)
 	}
 
-	if !rp.checkState() {
+	select {
+	case <-rp.shutdown:
+		rp.onClose()
 		return
+	default:
+		rp.out <- rp.parser.Parse(rp.timeout, url)
 	}
-
-	// Access rp.parser there because otherwise it will Data Race
-	parser := rp.parser
-	rp.wg.Add(1)
-	go func() {
-		// rp.parser !!! DATA RACE
-		data := parser.Parse(rp.timeout, url)
-		if !rp.checkState() {
-			// Return gracefully
-			// Do not write on potentially closed rp.out channel
-			rp.wg.Done()
-			return
-		}
-		rp.out <- data
-		rp.wg.Done()
-	}()
-
 }
 
-func (rp *RingParser) checkState() bool {
-	// Check if RingParser.Close() has been called. See RingParser.Close
-	if atomic.LoadUint32(&rp.state) == 0 {
-		// Stopped
-		return false
-	}
-
-	return true
+func (rp *RingParser) onClose() {
+	close(rp.out)
 }
--- internal/parser/ring_parser_test.go
+++ internal/parser/ring_parser_test.go
@@ -40,56 +40,28 @@ func TestRingParser(t *testing.T) {
 	})
 
 	t.Run("test can run, read, gracefully close", func(t *testing.T) {
-		t.Run("WITH extra 5ms for overhead (CHECK GRACEFUL CLOSE AND NO RACE CONDITIONS)", func(t *testing.T) {
-			ringParser := rpWithURLs()
-			// Add simple reader from output chan
-			var countUpdates int32
-			go func() {
-				for update := range ringParser.Out() {
-					require.EqualValues(t, mockParseResult, update)
-					atomic.AddInt32(&countUpdates, 1)
-				}
-			}()
+		ringParser := rpWithURLs()
+		// Add simple reader from output chan
+		var countUpdates int32
+		go func() {
+			for update := range ringParser.Out() {
+				require.EqualValues(t, mockParseResult, update)
+				atomic.AddInt32(&countUpdates, 1)
+			}
+		}()
 
-			// Spawns a goroutine under the hood
-			ringParser.Run(time.Second * 1 /* Parsing interval */)
+		// Spawns a goroutine under the hood
+		ringParser.Run(time.Second * 1 /* Parsing interval */)
 
-			// Sleep for abit longer than 2000ms(2s) because of some overhead
-			time.Sleep(time.Millisecond * 2005)
+		// Sleep for abit longer than 2000ms(2s) because of some overhead
+		time.Sleep(time.Millisecond * 2005)
 
-			ringParser.Close()
-			// Can run two complete parsings.
-			// ringParser.Run has interval - 1s
-			// Time asleep - 2.005s =>
-			// Two complete parsings could be performed
-			require.Equal(t, int32(2) /* expected count updates */, atomic.LoadInt32(&countUpdates))
-		})
-
-		t.Run("WITHOUT extra 5ms for overhead (CHECK GRACEFUL CLOSE AND NO RACE CONDITIONS)", func(t *testing.T) {
-			ringParser := rpWithURLs()
-			// Add simple reader from output chan
-			var countUpdates int32
-			go func() {
-				for update := range ringParser.Out() {
-					require.EqualValues(t, mockParseResult, update)
-					atomic.AddInt32(&countUpdates, 1)
-				}
-			}()
-
-			// Spawns a goroutine under the hood
-			ringParser.Run(time.Second * 1 /* Parsing interval */)
-
-			// Sleep exactly two seconds (ringParser cant handle millisecond to millisecond parsing)
-			time.Sleep(time.Millisecond * 2000)
-
-			ringParser.Close()
-			// Can run two complete parsings.
-			// ringParser.Run has interval - 1s
-			// Time asleep - 2.000s =>
-			// One parsing can be performed.
-			// ringParser has safe concurrency model and prevents data races
-			require.Equal(t, int32(1) /* expected count updates */, atomic.LoadInt32(&countUpdates))
-		})
+		ringParser.Close()
+		// Can run two complete parsings.
+		// ringParser.Run has interval - 1s
+		// Time asleep - 2.005s =>
+		// Two complete parsings could be performed
+		require.Equal(t, int32(2) /* expected count updates */, atomic.LoadInt32(&countUpdates))
 	})
 
 }
@@ -106,8 +78,6 @@ func rpWithURLs() *RingParser {
 	}
 
 	for _, url := range urls {
-		// prevents from adding same url twice so no effect
-		// testing purposes...
 		rp.AddTarget(url)
 	}
 
